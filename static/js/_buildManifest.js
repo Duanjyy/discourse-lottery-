ad0cbafca33937b3d3f18facf4baffcf76795c5a
汤圆 <91202296+Duanjyy@users.noreply.github.com>
2026-04-24 15:21:54 +0800

    11
    111

---
+++ static/js/_buildManifest.js
@@ -0,0 +1,772 @@
+﻿self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/f1409ab3a488fa7f.js"
+  ],
+  "/[...url]": [
+    "static/chunks/8c961b0457657697.js"
+  ],
+  "/[url]/claim": [
+    "static/chunks/35f571c56ce8efc2.js"
+  ],
+  "/[url]/transfer": [
+    "static/chunks/1ae5e5e9fefbbf63.js"
+  ],
+  "/__admin": [
+    "static/chunks/6161259d4df502ed.js"
+  ],
+  "/__admin/[adminFallback]": [
+    "static/chunks/e956c39bd8b7c114.js"
+  ],
+  "/__admin/appeals/[userApiId]": [
+    "static/chunks/10a2280500354ceb.js"
+  ],
+  "/__admin/appeals/calendar/[calendarApiId]": [
+    "static/chunks/05a7ace204fd065f.js"
+  ],
+  "/__admin/category/[...categorySlug]": [
+    "static/chunks/1ff71b7571119689.js"
+  ],
+  "/__admin/discover": [
+    "static/chunks/5dff3e51a78e2771.js"
+  ],
+  "/__admin/discover/[placeSlug]/[[...adminDiscoverTab]]": [
+    "static/chunks/2ac75043ae893a7c.js"
+  ],
+  "/__admin/discover/all-pending-events": [
+    "static/chunks/9a34d12f6b6063bd.js"
+  ],
+  "/__admin/domain": [
+    "static/chunks/c67b042078130787.js"
+  ],
+  "/__admin/emails/[marketingEmailApiId]": [
+    "static/chunks/68076f3582744235.js"
+  ],
+  "/__admin/event": [
+    "static/chunks/60e9473726618c38.js"
+  ],
+  "/__admin/event-list": [
+    "static/chunks/11bd3bb1e5affa2c.js"
+  ],
+  "/__admin/featured": [
+    "static/chunks/c67ceff705729dfe.js"
+  ],
+  "/__admin/gallery": [
+    "static/chunks/2ea6f55aec48d161.js"
+  ],
+  "/__admin/import-limit-requests/[requestApiId]": [
+    "static/chunks/879ef0795bb19b44.js"
+  ],
+  "/__admin/instagram/[[...instagramTab]]": [
+    "static/chunks/46162f3c647ecdda.js"
+  ],
+  "/__admin/jobs": [
+    "static/chunks/befcb07e9fdd882e.js"
+  ],
+  "/__admin/maintenance": [
+    "static/chunks/1994af123ccc1579.js"
+  ],
+  "/__admin/map": [
+    "static/chunks/9dec5072bf08808a.js"
+  ],
+  "/__admin/marketing-emails": [
+    "static/chunks/a0f4fd9a47ebf285.js"
+  ],
+  "/__admin/organizations": [
+    "static/chunks/d85b37a82c7cf163.js"
+  ],
+  "/__admin/push-notification": [
+    "static/chunks/5a0f546411b68631.js"
+  ],
+  "/__admin/region": [
+    "static/chunks/792da2e8b780738c.js"
+  ],
+  "/__admin/spam": [
+    "static/chunks/72362d4cba42dba3.js"
+  ],
+  "/__admin/spam/calendars": [
+    "static/chunks/8904d120293bf2a0.js"
+  ],
+  "/__admin/super": [
+    "static/chunks/d578e33e3e3e66be.js"
+  ],
+  "/__admin/themes": [
+    "static/chunks/42384405d854cbfa.js"
+  ],
+  "/__admin/verifications": [
+    "static/chunks/1659b323993bfc5a.js"
+  ],
+  "/__admin/verify/[verificationRequestId]": [
+    "static/chunks/fbdc3238796ea12e.js"
+  ],
+  "/__admin/whats-happening": [
+    "static/chunks/fa5f700d0a3f1cf8.js"
+  ],
+  "/__front": [
+    "static/chunks/a2ef315d058443fc.js"
+  ],
+  "/_crypto": [
+    "static/chunks/dc90a9bb5525416e.js"
+  ],
+  "/_crypto/choose-token": [
+    "static/chunks/cfe01adc3ade5fc5.js"
+  ],
+  "/_error": [
+    "static/chunks/24cf9ecf52fd4f54.js"
+  ],
+  "/appeals/calendar/[calendarApiId]": [
+    "static/chunks/768073445c4cf2a1.js"
+  ],
+  "/appeals/user/[flagKey]": [
+    "static/chunks/d57ed7d76c0bfdba.js"
+  ],
+  "/apple": [
+    "static/chunks/1aafc1939ad538b4.js"
+  ],
+  "/blast/[blastId]": [
+    "static/chunks/03ebccb364383141.js"
+  ],
+  "/calendar/[...calendarId]": [
+    "static/chunks/0948f24f3d93b9a8.js"
+  ],
+  "/calendar/[calendarId]/terms": [
+    "static/chunks/4ca1ed42614b1a85.js"
+  ],
+  "/calendar/connect-stripe/[calendarApiId]": [
+    "static/chunks/45431a4ab1f1df85.js"
+  ],
+  "/calendar/manage/[...manageCalendarApiId]": [
+    "static/chunks/e8ffe326ecebf026.js"
+  ],
+  "/calendar/manage/[calendarApiId]/drafts/[draftApiId]/compose": [
+    "static/chunks/375f8f26cef149e6.js"
+  ],
+  "/calendar/manage/[calendarApiId]/newsletters/[newsletterApiId]/edit": [
+    "static/chunks/10b3281c38e51714.js"
+  ],
+  "/calendar/manage/[calendarApiId]/verify": [
+    "static/chunks/3803b790256e4cd5.js"
+  ],
+  "/calendar/manage/api-keys": [
+    "static/chunks/4d26f870dc249c1a.js"
+  ],
+  "/calendar/manage/plus": [
+    "static/chunks/3a8cb020894a7111.js"
+  ],
+  "/category/[slug]": [
+    "static/chunks/f0b410c113371ebe.js"
+  ],
+  "/category/[slug]/map": [
+    "static/chunks/b7cf7af9e9878a79.js"
+  ],
+  "/chat": [
+    "static/chunks/07ac0d8e82362944.js"
+  ],
+  "/check-in/[...scanEventApiId]": [
+    "static/chunks/a4ee10591f27a9ee.js"
+  ],
+  "/christmas": [
+    "static/chunks/c6689ee7464fba97.js"
+  ],
+  "/create": [
+    "static/chunks/862f536aaf2a6114.js"
+  ],
+  "/create-calendar": [
+    "static/chunks/4e37b2250bf5019a.js"
+  ],
+  "/discover": [
+    "static/chunks/6e2abdb6f5c08961.js"
+  ],
+  "/discover/[...placeSlug]": [
+    "static/chunks/bbaddd2e917bb68f.js"
+  ],
+  "/diwali": [
+    "static/chunks/ad83d7a41c05c819.js"
+  ],
+  "/dpa": [
+    "static/chunks/8b67530d455a5cc2.js"
+  ],
+  "/e/[eventId]": [
+    "static/chunks/2d94dc9cc6281444.js"
+  ],
+  "/e/m/[keyForMap]": [
+    "static/chunks/3668ded122c9fd8f.js"
+  ],
+  "/e/r/[keyForPage]": [
+    "static/chunks/365798e449564215.js"
+  ],
+  "/e/ticket/[eventTicket]": [
+    "static/chunks/47f433b32139a0db.js"
+  ],
+  "/eclipse": [
+    "static/chunks/804539c7eacf6115.js"
+  ],
+  "/em/[...emPath]": [
+    "static/chunks/97de6d12f89435a0.js"
+  ],
+  "/embed-checkout/[embeddedEventApiId]": [
+    "static/chunks/545e74eff7e3d4f6.js"
+  ],
+  "/embed-events/[hostApiId]": [
+    "static/chunks/fe34fd74e67605e0.js"
+  ],
+  "/embed/calendar/[calendarApiId]/events": [
+    "static/chunks/b70e767ab3044d67.js"
+  ],
+  "/embed/event/[eventApiId]/simple": [
+    "static/chunks/63e36ae9f6706a2f.js"
+  ],
+  "/event/[eventId]": [
+    "static/chunks/146c0409e32ed4ff.js"
+  ],
+  "/event/[eventId]/claim": [
+    "static/chunks/c943fa87d02279c2.js"
+  ],
+  "/event/[eventId]/terms": [
+    "static/chunks/28a967432b5e9233.js"
+  ],
+  "/event/[eventId]/transfer": [
+    "static/chunks/576cd6a98511cbe4.js"
+  ],
+  "/event/manage/[...manageEventApiId]": [
+    "static/chunks/36b7754cea3c6534.js"
+  ],
+  "/event/manage/guest-table/[eventApiId]": [
+    "static/chunks/30473032f3a33875.js"
+  ],
+  "/eventbrite": [
+    "static/chunks/4abc26924a66579b.js"
+  ],
+  "/fall": [
+    "static/chunks/047dd6c2ba900d2b.js"
+  ],
+  "/floral": [
+    "static/chunks/f4212fbb83cdd547.js"
+  ],
+  "/google": [
+    "static/chunks/2685d24ae122a344.js"
+  ],
+  "/halloween": [
+    "static/chunks/8fd5c29fdf420c4c.js"
+  ],
+  "/hanukkah": [
+    "static/chunks/57c5439853c96fbf.js"
+  ],
+  "/holiday": [
+    "static/chunks/8a6444cce7b71fce.js"
+  ],
+  "/home": [
+    "static/chunks/56486e9ef9bf9635.js"
+  ],
+  "/home/calendars": [
+    "static/chunks/de6c2fbbe3c8b8ba.js"
+  ],
+  "/home2": [
+    "static/chunks/4514e07e5f0e2349.js"
+  ],
+  "/i/[eventInviteKey]": [
+    "static/chunks/91a416e719c587e7.js"
+  ],
+  "/invite/decline-host-invite": [
+    "static/chunks/7b8f4756e5ee7f01.js"
+  ],
+  "/invoice/[chargeId]": [
+    "static/chunks/425fcb30de9665ef.js"
+  ],
+  "/invoice/luma-plus/[stripeInvoiceId]": [
+    "static/chunks/7c93d3f0e74ef99b.js"
+  ],
+  "/join-check-in-staff/[eventId]": [
+    "static/chunks/92e58806d8d3739c.js"
+  ],
+  "/join/[proxyKey]": [
+    "static/chunks/6fbe49fd7ec3929a.js"
+  ],
+  "/july4": [
+    "static/chunks/17f9ff79aef8ccdd.js"
+  ],
+  "/map/e/[eventApiIdForMap]": [
+    "static/chunks/ad630ade9709d478.js"
+  ],
+  "/matrix": [
+    "static/chunks/f8adb498feface5e.js"
+  ],
+  "/me/[[...me]]": [
+    "static/chunks/1503d226f3f3c850.js"
+  ],
+  "/mobile/editor": [
+    "static/chunks/1428127b5119a690.js"
+  ],
+  "/mobile/open-ios": [
+    "static/chunks/ec5c1f79d3989bd3.js"
+  ],
+  "/notifications": [
+    "static/chunks/d7ccd53a853a7b1c.js"
+  ],
+  "/nye": [
+    "static/chunks/0e3106cded9a487e.js"
+  ],
+  "/oauth/consent": [
+    "static/chunks/744d1441b71ddbb9.js"
+  ],
+  "/okta/callback": [
+    "static/chunks/0b3dda8334754af7.js"
+  ],
+  "/okta/launch": [
+    "static/chunks/734395550ddd7a5c.js"
+  ],
+  "/organization/[organizationId]": [
+    "static/chunks/9556b8954e86d106.js"
+  ],
+  "/organization/manage/[...manageOrganizationId]": [
+    "static/chunks/7777811933f412a7.js"
+  ],
+  "/p/[...postId]": [
+    "static/chunks/7477173df3cb3e6f.js"
+  ],
+  "/personal/[[...manage]]": [
+    "static/chunks/4ed3aad3b83038ce.js"
+  ],
+  "/polaroid": [
+    "static/chunks/c04d06b588f9ebe5.js"
+  ],
+  "/pricing": [
+    "static/chunks/ca6445c2837dc4a7.js"
+  ],
+  "/privacy-policy": [
+    "static/chunks/db56aec65e92c5ba.js"
+  ],
+  "/release/fall-2023": [
+    "static/chunks/580f6fb58d993fa5.js"
+  ],
+  "/release/spring-2024": [
+    "static/chunks/3e8dfed4d590a6f4.js"
+  ],
+  "/reset-password": [
+    "static/chunks/00afd2e2b8817be2.js"
+  ],
+  "/security": [
+    "static/chunks/5b96d538bbb2e993.js"
+  ],
+  "/settings/[[...tab]]": [
+    "static/chunks/90fc00a09295e60b.js"
+  ],
+  "/signin": [
+    "static/chunks/3799edfad6b733c2.js"
+  ],
+  "/signout": [
+    "static/chunks/1393a31927a6c977.js"
+  ],
+  "/sms-terms": [
+    "static/chunks/78720f0fd78c20be.js"
+  ],
+  "/snow": [
+    "static/chunks/7d390ae9a95d3a6d.js"
+  ],
+  "/social-share": [
+    "static/chunks/3c20abec671feb25.js"
+  ],
+  "/st-patricks": [
+    "static/chunks/0265b2f85b6f8f9c.js"
+  ],
+  "/stripe/connect": [
+    "static/chunks/f083c1339043d9f4.js"
+  ],
+  "/style-guide/[[...example]]": [
+    "static/chunks/774bbdf02dc7997a.js"
+  ],
+  "/style-guide/drop-zone": [
+    "static/chunks/24e0555ffa8c1fbe.js"
+  ],
+  "/style-guide/empty-state": [
+    "static/chunks/5763441ec6f293c1.js"
+  ],
+  "/style-guide/notifications": [
+    "static/chunks/d9c75244d05c8fe4.js"
+  ],
+  "/style-guide/sortable": [
+    "static/chunks/d880402a5bef11ce.js"
+  ],
+  "/subprocessors": [
+    "static/chunks/f4a423d5f6fcead9.js"
+  ],
+  "/support": [
+    "static/chunks/0d5d8a96bc9b5ebb.js"
+  ],
+  "/support/lookup-card": [
+    "static/chunks/7e99e43a15729898.js"
+  ],
+  "/survey-response": [
+    "static/chunks/ccdc446fb1fb2b47.js"
+  ],
+  "/terms": [
+    "static/chunks/79049946a7219f42.js"
+  ],
+  "/test": [
+    "static/chunks/d4aa8dd4fc8a1074.js"
+  ],
+  "/test/avatars": [
+    "static/chunks/796a4f4f61ce3e2c.js"
+  ],
+  "/test/before-content": [
+    "static/chunks/4763c9bc8bb67264.js"
+  ],
+  "/test/cent": [
+    "static/chunks/a4aefbf33e1c207b.js"
+  ],
+  "/test/chat-search": [
+    "static/chunks/3ec19da827a48694.js"
+  ],
+  "/test/color-quantize": [
+    "static/chunks/00435781bacfa1ee.js"
+  ],
+  "/test/commit": [
+    "static/chunks/ae9e15eaa77e129c.js"
+  ],
+  "/test/css": [
+    "static/chunks/43abd38a245bfcc6.js"
+  ],
+  "/test/deep-links": [
+    "static/chunks/32c7e1addd726be8.js"
+  ],
+  "/test/deep/x": [
+    "static/chunks/74b44e8a6ec79d18.js"
+  ],
+  "/test/discover": [
+    "static/chunks/2d52e51630bfb480.js"
+  ],
+  "/test/emoji-list": [
+    "static/chunks/dbec2155241ef83d.js"
+  ],
+  "/test/emojis": [
+    "static/chunks/7d3de25253cb4f5a.js"
+  ],
+  "/test/error-debug/client": [
+    "static/chunks/06c1eea9c7d7afb2.js"
+  ],
+  "/test/error-debug/server": [
+    "static/chunks/3c97389a866ceafb.js"
+  ],
+  "/test/eth-token-gate": [
+    "static/chunks/d57a29288a0457e1.js"
+  ],
+  "/test/eth-wallet": [
+    "static/chunks/2e34ba8a2773e5bd.js"
+  ],
+  "/test/gallery-hue": [
+    "static/chunks/8585109c10440e43.js"
+  ],
+  "/test/gauge": [
+    "static/chunks/46578b29c47950d9.js"
+  ],
+  "/test/gif": [
+    "static/chunks/69942b59377d329e.js"
+  ],
+  "/test/image-drawer": [
+    "static/chunks/bf5278651c562860.js"
+  ],
+  "/test/landing-page": [
+    "static/chunks/b01c3932526dace7.js"
+  ],
+  "/test/mentions": [
+    "static/chunks/e16bc86bf5851f1b.js"
+  ],
+  "/test/my-location": [
+    "static/chunks/8bfb2679da2c6bb7.js"
+  ],
+  "/test/num-input": [
+    "static/chunks/42fd020c9e69e22d.js"
+  ],
+  "/test/particles": [
+    "static/chunks/10c4be5dd5bbe05d.js"
+  ],
+  "/test/photo-uploader": [
+    "static/chunks/aeba28e99ef1b0a8.js"
+  ],
+  "/test/polaroid": [
+    "static/chunks/e5b6c7b3c28ba0c3.js"
+  ],
+  "/test/progress": [
+    "static/chunks/4a240faa6413dc18.js"
+  ],
+  "/test/qrs": [
+    "static/chunks/5ee795a83e52f446.js"
+  ],
+  "/test/questions": [
+    "static/chunks/61fec206c287974d.js"
+  ],
+  "/test/recurrence": [
+    "static/chunks/c4887dd953a1957a.js"
+  ],
+  "/test/search": [
+    "static/chunks/543be7c85eb51596.js"
+  ],
+  "/test/shader": [
+    "static/chunks/811066974e7c9717.js"
+  ],
+  "/test/sms-consent": [
+    "static/chunks/181eb6938dbb8e67.js"
+  ],
+  "/test/social-image": [
+    "static/chunks/318d9a73a3565344.js"
+  ],
+  "/test/sort-grid": [
+    "static/chunks/6d540f6cbf9abac4.js"
+  ],
+  "/test/starburst": [
+    "static/chunks/b8a193e68806ffc9.js"
+  ],
+  "/test/sudo": [
+    "static/chunks/72b60fc29dc26511.js"
+  ],
+  "/test/tooltips": [
+    "static/chunks/ca866c3fe632ebdd.js"
+  ],
+  "/test/turnstile": [
+    "static/chunks/7418033c314b5da1.js"
+  ],
+  "/test/waitlist-banner": [
+    "static/chunks/d213ee91f8129674.js"
+  ],
+  "/thanksgiving": [
+    "static/chunks/32d8c907a0311017.js"
+  ],
+  "/u/[...userApiId]": [
+    "static/chunks/abbda3f8d10cc9f7.js"
+  ],
+  "/unsubscribe": [
+    "static/chunks/a95dae767614ce43.js"
+  ],
+  "/user/[...username]": [
+    "static/chunks/4a57dfa19aa21f56.js"
+  ],
+  "/verify-email": [
+    "static/chunks/3d101e36c1be3865.js"
+  ],
+  "/vote": [
+    "static/chunks/4789fee9ede1d2c0.js"
+  ],
+  "/zoom": [
+    "static/chunks/9f481eb45b488aaf.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/.well-known/apple-app-site-association",
+        "destination": "/api/well-known/apple-app-site-association"
+      },
+      {
+        "source": "/.well-known/assetlinks.json",
+        "destination": "/api/well-known/assetlinks.json"
+      },
+      {
+        "source": "/.well-known/webauthn",
+        "destination": "/api/well-known/webauthn"
+      },
+      {
+        "source": "/robots.txt",
+        "destination": "/api/robots"
+      },
+      {
+        "source": "/_next/:path*.js.map",
+        "destination": "/404"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/__admin",
+    "/__admin/appeals/calendar/[calendarApiId]",
+    "/__admin/appeals/[userApiId]",
+    "/__admin/category/[...categorySlug]",
+    "/__admin/discover",
+    "/__admin/discover/all-pending-events",
+    "/__admin/discover/[placeSlug]/[[...adminDiscoverTab]]",
+    "/__admin/domain",
+    "/__admin/emails/[marketingEmailApiId]",
+    "/__admin/event",
+    "/__admin/event-list",
+    "/__admin/featured",
+    "/__admin/gallery",
+    "/__admin/import-limit-requests/[requestApiId]",
+    "/__admin/instagram/[[...instagramTab]]",
+    "/__admin/jobs",
+    "/__admin/maintenance",
+    "/__admin/map",
+    "/__admin/marketing-emails",
+    "/__admin/organizations",
+    "/__admin/push-notification",
+    "/__admin/region",
+    "/__admin/spam",
+    "/__admin/spam/calendars",
+    "/__admin/super",
+    "/__admin/themes",
+    "/__admin/verifications",
+    "/__admin/verify/[verificationRequestId]",
+    "/__admin/whats-happening",
+    "/__admin/[adminFallback]",
+    "/__front",
+    "/_app",
+    "/_crypto",
+    "/_crypto/choose-token",
+    "/_error",
+    "/api/ai/suggest-event-description",
+    "/api/dev/sign-in",
+    "/api/rewire/__tests__/rewire.test",
+    "/api/rewire/[[...path]]",
+    "/api/robots",
+    "/api/test",
+    "/api/well-known/apple-app-site-association",
+    "/api/well-known/assetlinks.json",
+    "/api/well-known/webauthn",
+    "/appeals/calendar/[calendarApiId]",
+    "/appeals/user/[flagKey]",
+    "/apple",
+    "/blast/[blastId]",
+    "/calendar/connect-stripe/[calendarApiId]",
+    "/calendar/manage/api-keys",
+    "/calendar/manage/plus",
+    "/calendar/manage/[calendarApiId]/drafts/[draftApiId]/compose",
+    "/calendar/manage/[calendarApiId]/newsletters/[newsletterApiId]/edit",
+    "/calendar/manage/[calendarApiId]/verify",
+    "/calendar/manage/[...manageCalendarApiId]",
+    "/calendar/[calendarId]/terms",
+    "/calendar/[...calendarId]",
+    "/category/[slug]",
+    "/category/[slug]/map",
+    "/chat",
+    "/check-in/[...scanEventApiId]",
+    "/christmas",
+    "/create",
+    "/create-calendar",
+    "/discover",
+    "/discover/[...placeSlug]",
+    "/diwali",
+    "/dpa",
+    "/e/m/[keyForMap]",
+    "/e/r/[keyForPage]",
+    "/e/ticket/[eventTicket]",
+    "/e/[eventId]",
+    "/eclipse",
+    "/em/[...emPath]",
+    "/embed/calendar/[calendarApiId]/events",
+    "/embed/event/[eventApiId]/simple",
+    "/embed-checkout/[embeddedEventApiId]",
+    "/embed-events/[hostApiId]",
+    "/event/manage/guest-table/[eventApiId]",
+    "/event/manage/[...manageEventApiId]",
+    "/event/[eventId]",
+    "/event/[eventId]/claim",
+    "/event/[eventId]/terms",
+    "/event/[eventId]/transfer",
+    "/eventbrite",
+    "/fall",
+    "/floral",
+    "/google",
+    "/halloween",
+    "/hanukkah",
+    "/holiday",
+    "/home",
+    "/home/calendars",
+    "/home2",
+    "/i/[eventInviteKey]",
+    "/invite/decline-host-invite",
+    "/invoice/luma-plus/[stripeInvoiceId]",
+    "/invoice/[chargeId]",
+    "/join/[proxyKey]",
+    "/join-check-in-staff/[eventId]",
+    "/july4",
+    "/map/e/[eventApiIdForMap]",
+    "/matrix",
+    "/me/[[...me]]",
+    "/mobile/editor",
+    "/mobile/open-ios",
+    "/notifications",
+    "/nye",
+    "/oauth/consent",
+    "/okta/callback",
+    "/okta/launch",
+    "/organization/manage/[...manageOrganizationId]",
+    "/organization/[organizationId]",
+    "/p/[...postId]",
+    "/personal/[[...manage]]",
+    "/polaroid",
+    "/pricing",
+    "/privacy-policy",
+    "/release/fall-2023",
+    "/release/spring-2024",
+    "/reset-password",
+    "/security",
+    "/settings/[[...tab]]",
+    "/signin",
+    "/signout",
+    "/sms-terms",
+    "/snow",
+    "/social-share",
+    "/st-patricks",
+    "/stripe/connect",
+    "/style-guide/drop-zone",
+    "/style-guide/empty-state",
+    "/style-guide/notifications",
+    "/style-guide/sortable",
+    "/style-guide/[[...example]]",
+    "/subprocessors",
+    "/support",
+    "/support/lookup-card",
+    "/survey-response",
+    "/terms",
+    "/test",
+    "/test/avatars",
+    "/test/before-content",
+    "/test/cent",
+    "/test/chat-search",
+    "/test/color-quantize",
+    "/test/commit",
+    "/test/css",
+    "/test/deep/x",
+    "/test/deep-links",
+    "/test/discover",
+    "/test/emoji-list",
+    "/test/emojis",
+    "/test/error-debug/client",
+    "/test/error-debug/server",
+    "/test/eth-token-gate",
+    "/test/eth-wallet",
+    "/test/gallery-hue",
+    "/test/gauge",
+    "/test/gif",
+    "/test/image-drawer",
+    "/test/landing-page",
+    "/test/mentions",
+    "/test/my-location",
+    "/test/num-input",
+    "/test/particles",
+    "/test/photo-uploader",
+    "/test/polaroid",
+    "/test/progress",
+    "/test/qrs",
+    "/test/questions",
+    "/test/recurrence",
+    "/test/search",
+    "/test/shader",
+    "/test/sms-consent",
+    "/test/social-image",
+    "/test/sort-grid",
+    "/test/starburst",
+    "/test/sudo",
+    "/test/tooltips",
+    "/test/turnstile",
+    "/test/waitlist-banner",
+    "/thanksgiving",
+    "/u/[...userApiId]",
+    "/unsubscribe",
+    "/user/[...username]",
+    "/verify-email",
+    "/vote",
+    "/zoom",
+    "/[url]/claim",
+    "/[url]/transfer",
+    "/[...url]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
